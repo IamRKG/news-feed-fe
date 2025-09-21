7948a44d89e604f12bf8f9689edb81fe0023a79d
IamRKG <radhacreative@gmail.com>
2025-09-21 14:10:55 +0530

    mf_plain

---
+++ mf_consumer/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css';
+import Provider from 'provider';
+
+const App = () => {
+  return (
+    <div className="content">
+       <h1 className="title">I am from Shell</h1>
+      <Provider />
+     
+    </div>
+  );
+};
+
+export default App;
